f93b55a98bbcb5ec9ed5d90cb4cffdd236d73fec
Maalim98 <ashrafzani585@gmail.com>
2025-01-04 12:20:58 +0300

    correct image path for succesful deploymen on vercel

--- src/components/ImpactStoriesSection.jsx
+++ src/components/ImpactStoriesSection.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 
-import woman1 from "../assets/images/wuman2.jpg";
+import woman1 from "../assets/images/slide3.jpg";
 import woman4 from "../assets/images/wuman4.jpg"; // Assuming the correct file name
 import xalimo from "../assets/images/halimo.jpg"; // Using xalimo.jpg as a fallback for missing images
 
