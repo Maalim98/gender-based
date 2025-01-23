7b807d074843829166be914f0c76c76fb3f0f404
Maalim98 <ashrafzani585@gmail.com>
2025-01-23 12:30:08 +0300

    Remove duplicate src folder

--- gbv/src/components/layout/ResponsiveContainer.jsx
+++
@@ -1,9 +0,0 @@
-const ResponsiveContainer = ({ children, className = '' }) => {
-  return (
-    <div className={`container mx-auto px-4 ${className}`}>
-      {children}
-    </div>
-  );
-};
-
-export default ResponsiveContainer; 
